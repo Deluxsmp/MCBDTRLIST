406a12bc5fea9ba3594780c4dd52033cff725ffc
Vortextier <dhrubodhrubo282@gmail.com>
2026-07-17 14:22:50 +0600

    Create ​script.js

---
+++ ​script.js
@@ -0,0 +1,15 @@
+<script>
+    function showWebsite() {
+        document.getElementById('landing-page').style.display = 'none';
+        document.getElementById('main-content').style.display = 'block';
+    }
+
+    function liveSearch() {
+        var input = document.getElementById('tierSearch').value.toUpperCase();
+        var rows = document.getElementsByClassName('player-row-item');
+        for (var i = 0; i < rows.length; i++) {
+            var name = rows[i].getElementsByClassName('player-name')[0].innerText;
+            rows[i].style.display = name.toUpperCase().indexOf(input) > -1 ? "" : "none";
+        }
+    }
+</script>
